672dac0f971c15ed8c9442b23dadc87044474eb7
Michelle Duggan <michelle.duggan@ucdconnect.ie>
2021-06-20 14:18:58 +0100

    class made for routes table. linked to db

--- map/models.py
+++ map/models.py
@@ -15,3 +15,23 @@ class BikeStation(models.Model):
     bk_available_bikes = models.IntegerField()
     bk_status = models.CharField(max_length = 45)
     bk_last_update = models.CharField(max_length = 45)
+
+
+class AllBusRoutes(models.Model):
+    shapeid = models.CharField(db_column='ShapeID', max_length=20, blank=True, null=True)  # Field name made lowercase.
+    stopsequence = models.IntegerField(db_column='StopSequence', blank=True, null=True)  # Field name made lowercase.
+    routename = models.CharField(db_column='RouteName', max_length=10, blank=True, null=True)  # Field name made lowercase.
+    routedescription = models.CharField(db_column='RouteDescription', max_length=40, blank=True, null=True)  # Field name made lowercase.
+    direction = models.CharField(db_column='Direction', max_length=5, blank=True, null=True)  # Field name made lowercase.
+    latitude = models.FloatField(db_column='Latitude', blank=True, null=True)  # Field name made lowercase.
+    longitude = models.FloatField(db_column='Longitude', blank=True, null=True)  # Field name made lowercase.
+    shortcommonname_en = models.CharField(db_column='ShortCommonName_en', max_length=40, blank=True, null=True)  # Field name made lowercase.
+    haspole = models.CharField(db_column='HasPole', max_length=20, blank=True, null=True)  # Field name made lowercase.
+    hasshelter = models.CharField(db_column='HasShelter', max_length=20, blank=True, null=True)  # Field name made lowercase.
+    routedata = models.CharField(db_column='RouteData', max_length=50, blank=True, null=True)  # Field name made lowercase.
+    route = models.CharField(db_column='Route', max_length=10)  # Field name made lowercase.
+    description = models.CharField(db_column='Description', max_length=45, blank=True, null=True)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'all_bus_routes'
