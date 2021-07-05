184ffdfacae96b55c491504847671b18b81d1e30
Michelle Duggan <michelle.duggan@ucdconnect.ie>
2021-07-05 11:37:27 +0100

    faulty merge fixed

--- map/views.py
+++ map/views.py
@@ -13,10 +13,8 @@ def index(request):
 	form1 = au.UserForm()
 	form2 = au.AuthForm()
 	return render(request, 'index.html', {"form1": form1, "form2": form2})
-
 def BusStation(request):
 	ret = BusStops.objects.values('stop_name','stop_lat','stop_long').distinct()
-	ret = MapAllroutedata.objects.values('shortcommonname_en','latitude','longitude').distinct()
 	data = list(ret)
 	data = json.dumps(data)
 	return HttpResponse(data)
@@ -24,15 +22,9 @@ def BusStation(request):
 def RouteDirection(request):
 	start_stop = request.GET.get("start_stop","")
 	end_stop = request.GET.get("end_stop","")
-	date = request.GET.get("date","")
-	time = request.GET.get("time","")
 	ret_start_stop = BusStops.objects.values('stop_name','stop_lat','stop_long').filter(stop_name = start_stop).distinct()
 	ret_end_stop = BusStops.objects.values('stop_name','stop_lat','stop_long').filter(stop_name = end_stop).distinct()
 	ret = chain(ret_start_stop, ret_end_stop)
-	ret_start_stop = MapAllroutedata.objects.values('shortcommonname_en','latitude','longitude').filter(shortcommonname_en = start_stop).distinct()
-	ret_end_stop = MapAllroutedata.objects.values('shortcommonname_en','latitude','longitude').filter(shortcommonname_en = end_stop).distinct()
-	ret = ret_start_stop | ret_end_stop
-
 	data = list(ret)
 	data = json.dumps(data)
 	return HttpResponse(data)
