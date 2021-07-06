fdd289e89e0e18203a5d971913c2e94ee9475ba6
ErilMChen <mengxiaohei@foxmail.com>
2021-07-06 17:07:51 +0100

    add favorite stop user interface

--- map/models.py
+++ map/models.py
@@ -1,17 +1,17 @@
 from django.db import models
 
 # Create your models here.
-class BikeStation(models.Model):
-    bk_id = models.IntegerField(primary_key=True, null = False)
-    bk_contract_name = models.CharField(max_length = 45)
-    bk_name = models.CharField(max_length = 45)
-    bk_address = models.CharField(max_length = 45)
-    bk_lat = models.FloatField()
-    bk_lng = models.FloatField()
-    bk_banking = models.CharField(max_length = 45)
-    bk_bouns = models.CharField(max_length = 45)
-    bk_bike_stands = models.IntegerField()
-    bk_available_bike_stands = models.IntegerField()
-    bk_available_bikes = models.IntegerField()
-    bk_status = models.CharField(max_length = 45)
-    bk_last_update = models.CharField(max_length = 45)
+class BusStops(models.Model):
+    stoppointid = models.IntegerField()
+    stop_name = models.CharField(max_length = 45)
+    stop_lat = models.FloatField()
+    stop_long = models.FloatField()
+    direction = models.IntegerField()
+    routes_serving = models.TextField()
+
+class NameToID(models.Model):
+    stop_id = models.CharField(max_length = 45)
+    stop_name = models.CharField(max_length = 45)
+    stop_lat = models.FloatField()
+    stop_long = models.FloatField()
+    stop_num = models.IntegerField()
--- map/views.py
+++ map/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from itertools import chain
 from django.http import HttpResponse, JsonResponse
 from django.core import serializers
 from .models import *
@@ -8,7 +9,36 @@ import json
 def index(request):
     return render(request, 'index.html')
 
-def bikestation(request):
-	ret = BikeStation.objects.all()
-	data = serializers.serialize("json", ret, ensure_ascii=False)
+def BusStation(request):
+	ret = BusStops.objects.values('stop_name','stop_lat','stop_long').distinct()
+	data = list(ret)
+	data = json.dumps(data)
 	return HttpResponse(data)
+
+def RouteDirection(request):
+	start_stop = request.GET.get("start_stop","")
+	end_stop = request.GET.get("end_stop","")
+	date = request.GET.get("date","")
+	time = request.GET.get("time","")
+	ret_start_stop = BusStops.objects.values('stop_name','stop_lat','stop_long').filter(stop_name = start_stop).distinct()
+	ret_end_stop = BusStops.objects.values('stop_name','stop_lat','stop_long').filter(stop_name = end_stop).distinct()
+	ret = chain(ret_start_stop, ret_end_stop)
+	data = list(ret)
+	data = json.dumps(data)
+	return HttpResponse(data)
+
+def AddFavoriteStop(request):
+	stop = request.GET.get("stop_name","")
+	ret1 = BusStops.objects.values('stop_name','routes_serving').filter(stop_name = stop).distinct()
+	ret2 = NameToID.objects.values('stop_name','stop_id').filter(stop_name = stop).distinct()
+	data1 = list(ret1)
+	data2 = list(ret2)
+
+	stop_name = data1[0]['stop_name']
+	print(stop_name)
+	route_nums = data1[0]['routes_serving'].split(',')
+	print(route_nums)
+	stop_id = data2[0]['stop_id']
+	print(stop_id)
+
+	return HttpResponse(data1)
