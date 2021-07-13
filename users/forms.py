d31c83c70e0988978b932ef81437060493991318
Michelle Duggan <michelle.duggan@ucdconnect.ie>
2021-07-13 17:07:17 +0100

    name field added on form

--- users/forms.py
+++ users/forms.py
@@ -2,15 +2,17 @@ from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django import forms
 from .models import MyUser
+
 #from django.contrib.auth.models import User
 
 class UserForm(UserCreationForm):
     """This is the form used to create MyUsers"""
     email = forms.EmailField()
 
+
     class Meta:
         model = MyUser
-        fields = ["email", "password1"]
+        fields = ["email", "name", "password1"]
 
 class AuthForm(AuthenticationForm):
     """This is the form used to log in MyUsers"""
