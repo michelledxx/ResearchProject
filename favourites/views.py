47cff7b7a9f5a33f5df4fb27e5e029889d71ab23
Michelle Duggan <michelle.duggan@ucdconnect.ie>
2021-07-20 19:14:32 +0100

    password change form

--- favourites/views.py
+++ favourites/views.py
@@ -1,17 +1,20 @@
 from django.shortcuts import render, redirect, HttpResponse, HttpResponseRedirect
-
+from django.http import JsonResponse
 from users.models import my_stations
 # Create your views here.
 from favourites import get_sched2
 from django.views.decorators.csrf import csrf_exempt
 from .forms import StopForm as S
+from users.forms import ChangePassword
+from users.models import MyUser
 
 
 
 def stations(request):
     """Sends the delete stop form to stations.html and render template"""
     myform = S
-    return render(request, 'mystations.html', {"form1": myform})
+    change_pass = ChangePassword
+    return render(request, 'mystations.html', {"form1": myform, "form2": change_pass})
 
 def check_auth(request):
     """Checks if user is authenticated. (((( NOT NEEDED))))"""
@@ -36,7 +39,7 @@ def show_favs(request):
         stations = my_stations.objects.filter(user=current_user).values_list('stop_id', flat=True).distinct()
         s = get_sched2.get_times(stations)
         data.append(s)
-        print(data)
+        #print(data)
         return HttpResponse(data, "application/json")
 
 @csrf_exempt
@@ -48,10 +51,23 @@ def delete_my_stop(request):
         station = postdata.get('name', '')
         delete_stop(station, current_user)
         print(station, "is station")
-    myform = S
     return HttpResponseRedirect('/mystations/')
 
 def delete_stop(station, user):
     """Deletes the stop"""
     my_stations.objects.filter(user=user, stop_id=station).delete()
     return
+
+def change_password(request):
+    if request.method == 'POST':
+        current_user = request.user
+        form = ChangePassword(request.POST)
+        if form.is_valid():
+            new_p = form.clean_confirm_password()
+            u = MyUser.objects.get(id=current_user.id)
+            u.set_password(new_p)
+            u.save()
+            return HttpResponse('Password has been changed')
+        else:
+            print('invalid')
+    return HttpResponse('Password has not been changed')
--- users/forms.py
+++ users/forms.py
@@ -2,8 +2,12 @@ from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.core.exceptions import ValidationError
 from django.contrib.auth.views import LoginView
+from django.contrib.auth.forms import (
+    AuthenticationForm, PasswordChangeForm, PasswordResetForm, SetPasswordForm,
+)
 from django import forms
 from .models import MyUser
+from django.contrib.auth.hashers import make_password, check_password
 
 #from django.contrib.auth.models import User
 
@@ -28,3 +32,21 @@ class MyLoginView(LoginView):
     authentication_form = AuthForm
 
 
+class ChangePassword(PasswordResetForm):
+    old_password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'type':'password'}))
+    new_password =  forms.CharField(widget=forms.PasswordInput(attrs={ 'class': 'form-control','type':'password'}))
+    reenter_password =  forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control','type':'password'}))
+
+    def clean_confirm_password(self):
+        cleaned_data = super(ChangePassword, self).clean()
+        new_password = self.cleaned_data.get('new_password')
+        reenter_password = self.cleaned_data.get('reenter_password')
+        old_password = self.cleaned_data.get('old_password')
+
+        if new_password and new_password != reenter_password or new_password == old_password or len(new_password) < 8:
+            raise ValidationError("Passwords are not matching or have been used before. Try again.")
+        return new_password
+
+    class Meta:
+        model = MyUser
+        fields = ['old_password', 'new_password', 'reenter_password']
